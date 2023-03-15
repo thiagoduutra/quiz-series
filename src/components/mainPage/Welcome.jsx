5c238f328d496ea079f4b9a39eacf487c5ca035d
ThiagoDuutra <thiagoduutra01@gmail.com>
2023-03-15 11:23:39 -0300

    feat#2: Cria estrutura context com Reducer

--- src/components/mainPage/Welcome.jsx
+++ src/components/mainPage/Welcome.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import { useContext } from "react";
+import { QuizContext } from "../../context/quiz";
 import Quiz from "../../img/quiz.png";
 import "./Welcome.scss";
 
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="container-welcome">
       <h2 className="subTitle">Seja Bem-vindo</h2>
       <p className="paragraph">Clique no botão abaixo para começar</p>
-      <button className="btn">Iniciar</button>
+      <button
+        onClick={() => dispatch({ type: "CHANGE_STATE" })}
+        className="btn"
+      >
+        Iniciar
+      </button>
       <div className="container-img">
         <img src={Quiz} alt="" />
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QuizProvider } from "./context/quiz";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </React.StrictMode>
 );
