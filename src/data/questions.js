8a33854d04a6d6d712ea1d1d2cb6c093fc2f7769
ThiagoDuutra <thiagoduutra01@gmail.com>
2023-03-15 14:09:23 -0300

    feat#05: Finalizado

--- src/data/questions.js
+++ src/data/questions.js
@@ -10,7 +10,7 @@ const data = [
     answer: "Joey Badass",
   },
   {
-    question: "Qual é o nome do ator que interpreta Elliot?",
+    question: "Qual é o nome do ator que interpreta o Elliot?",
     options: [
       "Christian Slater",
       "Rami Malek",
@@ -18,6 +18,61 @@ const data = [
       "Elliot Villar",
     ],
     answer: "Rami Malek",
+  },
+  {
+    question: "Qual era a profissão de Elliot?",
+    options: [
+      "DBA",
+      "Engenheiro de Software",
+      "Engenheiro de Redes",
+      "Engenheiro mecânico",
+    ],
+    answer: "Engenheiro de Software",
+  },
+  {
+    question: "onde Elliot encontrou o corpo de Shayla?",
+    options: [
+      "Na Banheira",
+      "No apartamento dele",
+      "No porta malas do carro",
+      "Enterrado na floresta",
+    ],
+    answer: "No porta malas do carro",
+  },
+  {
+    question: "Como Tyrell matou Sharon Knowles?",
+    options: [
+      "Estrangulou",
+      "Com uma faca",
+      "Ele a empurrou do telhado",
+      "Não foi Tyrell",
+    ],
+    answer: "Estrangulou",
+  },
+  {
+    question: "Qual nome do cachorrinho de Elliot?",
+    options: ["Snowball", "Python", "Flipper", "Cute"],
+    answer: "Flipper",
+  },
+  {
+    question: "Como Gideon morre?",
+    options: ["Suicídio", "Acidente de carro", "Tiro no pescoço", "Afogado"],
+    answer: "Tiro no pescoço",
+  },
+  {
+    question: "O que o hacker 5/9 fez?",
+    options: [
+      "Redes móveis trabadas nos EUA",
+      "Dados criptografados da E Corp",
+      "Apagou a dívida de todos",
+      "Vazou dados de pessoas públicas",
+    ],
+    answer: "Dados criptografados da E Corp",
+  },
+  {
+    question: "Onde estava Elliot na primeira metade da segunda temporada?",
+    options: ["Em sua casa", "Na prisão", "Na casa de sua mãe", "Manicômio"],
+    answer: "Na prisão",
   },
 ];
 
